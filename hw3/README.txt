1464f7f7e16115ba0bae073dd3c1c330f492122c
zcunique <zengchen94@gmail.com>
2018-02-07 13:40:06 -0600

    first commit of hw3

---
+++ hw3/README.txt
@@ -0,0 +1,7 @@
+#README
+
+The coding part has been coded except for function softmax_loss_vectorized in softmax.py and functions for extra credit.
+
+However, my PC cannot load in the dataset due to memory error. Decrease the loaded-in batch num in utils.py may avoid this problem. Last time it works, but it doesn't work this time and I don't know why. You may need to test it. And results need to be put on writeup.pdf.
+
+The previous 5 theory problem has been finished and I suggest you can do the coding part at first.
---
+++ hw3/softmax.py
@@ -0,0 +1,68 @@
+import numpy as np
+from random import shuffle
+import scipy.sparse
+
+def softmax_loss_naive(theta, X, y, reg):
+  """
+  Softmax loss function, naive implementation (with loops)
+  Inputs:
+  - theta: d x K parameter matrix. Each column is a coefficient vector for class k
+  - X: m x d array of data. Data are d-dimensional rows.
+  - y: 1-dimensional array of length m with labels 0...K-1, for K classes
+  - reg: (float) regularization strength
+  Returns:
+  a tuple of:
+  - loss as single float
+  - gradient with respect to parameter matrix theta, an array of same size as theta
+  """
+  # Initialize the loss and gradient to zero.
+
+  J = 0.0
+  grad = np.zeros_like(theta)
+  m, dim = X.shape
+
+  #############################################################################
+  # TODO: Compute the softmax loss and its gradient using explicit loops.     #
+  # Store the loss in J and the gradient in grad. If you are not              #
+  # careful here, it is easy to run into numeric instability. Don't forget    #
+  # the regularization term!                                                  #
+  #############################################################################
+  for i in range(0, m):
+    for j in range(0, K):
+      P = np.exp(theta[:, j].dot(X[i])) / np.sum(np.exp(theta.T.dot(X[i])))
+      J = J - int(y[i] == j) * np.log(P) / m
+      grad[:, j] = grad[:, j] + X[i] * (int(y[i] == j) - P) / m
+  J += reg * np.sum(theta * theta) / (2 * m)
+  grad += reg * theta / m
+
+  #############################################################################
+  #                          END OF YOUR CODE                                 #
+  #############################################################################
+
+  return J, grad
+
+  
+def softmax_loss_vectorized(theta, X, y, reg):
+  """
+  Softmax loss function, vectorized version.
+  Inputs and outputs are the same as softmax_loss_naive.
+  """
+  # Initialize the loss and gradient to zero.
+
+  J = 0.0
+  grad = np.zeros_like(theta)
+  m, dim = X.shape
+
+  #############################################################################
+  # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
+  # Store the loss in J and the gradient in grad. If you are not careful      #
+  # here, it is easy to run into numeric instability. Don't forget the        #
+  # regularization term!                                                      #
+  #############################################################################
+
+
+  #############################################################################
+  #                          END OF YOUR CODE                                 #
+  #############################################################################
+
+  return J, grad
